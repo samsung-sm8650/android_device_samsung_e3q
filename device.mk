8baac4ee462fa11f9f6d8dc2e38afe6824af50bf
David Wronek <david@mainlining.org>
2024-08-17 09:59:57 +0200

    e3q: Add HIDL

--- device.mk
+++ device.mk
@@ -34,6 +34,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0.vendor \
     android.hardware.health@2.1.vendor
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor \
