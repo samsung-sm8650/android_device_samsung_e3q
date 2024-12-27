4c8ad10bb500bf50261476dddd2f39fc0f4ecf48
David Wronek <david@mainlining.org>
2024-12-27 18:00:14 +0100

    e3q: Update fingerprints
    Change-Id: I9b79e02fcc0a8f4209b62c1833dd4a1308bd2401

--- lineage_e3q.mk
+++ lineage_e3q.mk
@@ -23,5 +23,5 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="e3qxxx-user 14 UP1A.231005.007 S928BXXS3AXI8 release-keys" \
-    BuildFingerprint="samsung/e3qxxx/qssi_64:14/UP1A.231005.007/S928BXXS3AXI8:user/release-keys"
+    BuildDesc="e3qxxx-user 14 UP1A.231005.007 S928BXXU4AXK4 release-keys" \
+    BuildFingerprint="samsung/e3qxxx/e3q:14/UP1A.231005.007/S928BXXU4AXK4:user/release-keys"
