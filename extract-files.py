5b706b31ef2f57b2a5210cc4364046438d83ff36
David Wronek <david@mainlining.org>
2025-04-27 13:52:11 +0200

    e3q: Update to S928BXXU4BYD9

--- extract-files.py
+++ extract-files.py
@@ -53,8 +53,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libskeymint_cli.so': blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
-    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
-        .add_line_if_missing('gettid: 1'),
+    #'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
+    #    .add_line_if_missing('gettid: 1'),
     'vendor/lib64/libsec-ril.so': blob_fixup()
         .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
         # mov x3, x21 -> mov x3, #0
