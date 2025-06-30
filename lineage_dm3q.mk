28fb18a635938dd8571d08c260c8fe365af81f9d
luk1337 <priv.luk@gmail.com>
2025-06-30 09:49:44 -0300

    dm3q: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_dm3q.mk
+++ lineage_dm3q.mk
@@ -23,6 +23,7 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc=$(call normalize-path-list, "dm3qxxx-user 15 AP3A.240905.015.A2 S918BXXS8DYF2 release-keys")
-
-BUILD_FINGERPRINT := samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXS8DYF2:user/release-keys
+    BuildDesc="dm3qxxx-user 15 AP3A.240905.015.A2 S918BXXS8DYF2 release-keys" \
+    BuildFingerprint=samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXS8DYF2:user/release-keys \
+    DeviceProduct=dm3qxxx \
+    SystemName=dm3qxxx
