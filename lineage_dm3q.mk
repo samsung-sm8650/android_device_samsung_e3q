7bb32a4f944b82dc56c55602bb7a949b1e58af62
Mezaque Silver <mezaquegit@gmail.com>
2025-07-06 19:40:59 -0300

    dm3q: Update blobs from S9180ZHS6DYF1 * Switch to Tawaneza variant, to match the main chapsand tree
    Change-Id: Iab7ca0ee52a5f1add580748d5ce0ccc461ab6c14

--- lineage_dm3q.mk
+++ lineage_dm3q.mk
@@ -17,13 +17,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_dm3q
 PRODUCT_DEVICE := dm3q
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := SM-S918B
+PRODUCT_MODEL := SM-S9180
 PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="dm3qxxx-user 15 AP3A.240905.015.A2 S918BXXS8DYF2 release-keys" \
-    BuildFingerprint=samsung/dm3qxxx/dm3q:15/AP3A.240905.015.A2/S918BXXS8DYF2:user/release-keys \
-    DeviceProduct=dm3qxxx \
-    SystemName=dm3qxxx
+    BuildDesc="dm3qzhx-user 15 AP3A.240905.015.A2 S9180ZHS6DYF1 release-keys" \
+    BuildFingerprint=samsung/dm3qzhx/dm3q:15/AP3A.240905.015.A2/S9180ZHS6DYF1:user/release-keys \
+    DeviceProduct=dm3qzhx \
+    SystemName=dm3qzhx
