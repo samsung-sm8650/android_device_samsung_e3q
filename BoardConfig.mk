8a2c694db5b07261b367af6009a047276cb2bcb6
David Wronek <david.wronek@mainlining.org>
2026-02-08 02:13:19 +0100

    e3q: Setup external modules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,8 +36,11 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/mm-drivers/hw_fence \
     qcom/opensource/mm-drivers/msm_ext_display \
     qcom/opensource/mm-drivers/sync_fence \
+    qcom/opensource/securemsm-kernel \
     qcom/opensource/audio-kernel \
+    qcom/opensource/synx-kernel \
     qcom/opensource/camera-kernel \
+    qcom/opensource/datarmnet-ext/mem \
     qcom/opensource/dataipa/drivers/platform/msm \
     qcom/opensource/datarmnet/core \
     qcom/opensource/datarmnet-ext/aps \
@@ -47,14 +50,18 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/datarmnet-ext/perf_tether \
     qcom/opensource/datarmnet-ext/sch \
     qcom/opensource/datarmnet-ext/wlan \
-    qcom/opensource/securemsm-kernel \
     qcom/opensource/display-drivers/msm \
+    qcom/opensource/dsp-kernel \
     qcom/opensource/eva-kernel \
     qcom/opensource/video-driver \
     qcom/opensource/graphics-kernel \
     qcom/opensource/wlan/platform \
     qcom/opensource/wlan/qcacld-3.0/.kiwi_v2 \
-    qcom/opensource/bt-kernel
+    qcom/opensource/bt-kernel \
+    qcom/opensource/spu-kernel \
+    qcom/opensource/mm-sys-kernel/ubwcp \
+    qcom/opensource/touch-drivers \
+    nxp/opensource/driver
 
 # Vendor props
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
